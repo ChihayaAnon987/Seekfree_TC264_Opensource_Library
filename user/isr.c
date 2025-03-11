258ab7c38e1ad55bbc5233dc3b2f4f601c912919
ChihayaAnon987 <2048324682@qq.com>
2025-03-11 12:51:42 +0800

    Version 5.3.1 通过条件编译控制开环闭环

--- user/isr.c
+++ user/isr.c
@@ -69,8 +69,11 @@ IFX_INTERRUPT(cc60_pit_ch1_isr, 0, CCU6_0_CH1_ISR_PRIORITY)
     {
         PDLocServoCtrl();                              // 舵机 PD位置式控制
         // PIDIncMotorCtrl(Test_Encoder);                 // 电机 PID增量式控制
-        // PIDIncMotorCtrl(Target_Encoder);               // 电机 PID增量式控制
+    #if MOTOR_LOOP_ENABLE == 1
+        PIDIncMotorCtrl(Target_Encoder);               // 电机 PID增量式控制
+    #elif MOTOR_LOOP_ENABLE == 0
         DRV8701_MOTOR_DRIVER(Target_Encoder);          // 电机驱动
+    #endif
     }
     #if UART_RECEIVER_ENABLE == 1
         if(Control_Flag == 1 && Center_Flag == 1)
